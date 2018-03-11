5e48da009ab0ad47f7732b9fc23a00a433e205f9
Leo Furze-Waddock <leo@furze-waddock.com>
2018-03-11 16:44:25 +0000

    add sample/silly src files

---
+++ src/components/greeting/index.js
@@ -0,0 +1,14 @@
+import { cube } from "../../modules/math";
+
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = [
+    "Hello js-tdd-kit TEST!",
+    `5 cubed is equal to ${cube(5)}`,
+  ].join("\n\n");
+
+  return element;
+}
+
+document.body.appendChild(component());
