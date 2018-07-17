b56b25fadeb53e276f998598420fac9ccab6d100
Leo Furze-Waddock <leo@furze-waddock.com>
2018-07-17 19:22:04 +0100

    allow 'src' index to load outside of a browser

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@
 import * as greeting from "./components/greeting";
 import { cube } from "./modules/math";
 
+function render(globalObj) {
+  const element = greeting.getElement(globalObj.document);
+  const mutateEl = greeting.component(element, html);
+  greeting.default(mutateEl, globalObj.document);
+}
+
 const cubed = cube(5);
 const html = greeting.generateHtml`Hello js-tdd-kit TEST!, 5 cubed is equal to ${cubed}`;
-if (typeof window.document !== "undefined") {
-  const element = greeting.getElement(window.document);
-  const mutateEl = greeting.component(element, html);
-  greeting.default(mutateEl, window.document);
+if (typeof window !== "undefined") {
+  render(window);
 }
