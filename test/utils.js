e4746ffe058d7080e66f1bf2406feb3c9a097a5a
Leo Furze-Waddock <leo@furze-waddock.com>
2018-07-17 15:38:56 +0100

    change isNodeEnvironment method

--- test/utils.js
+++ test/utils.js
@@ -1,19 +1,4 @@
 export default function isNodeEnvironment() {
-  // Check if current environment is Node, or other CommonJS-like environment
-  // Source: lodash codebase
-  // https://raw.githubusercontent.com/lodash/lodash/4.17.4/dist/lodash.core.js
-
-  if (
-    typeof global === "object" &&
-    global &&
-    global.Object === Object &&
-    global &&
-    typeof exports === "object" &&
-    exports &&
-    !exports.nodeType &&
-    exports
-  ) {
-    return true;
-  }
-  return false;
+  // Check if current environment is Node
+  return (new Function("try {return this===global;}catch(e){return false;}"))(); // eslint-disable-line no-new-func
 }
