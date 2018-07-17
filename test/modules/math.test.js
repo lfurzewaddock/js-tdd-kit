66ec4688cfd0ec180c4f0ea418b743b6c1ab72e2
Leo Furze-Waddock <leo@furze-waddock.com>
2018-07-17 15:05:49 +0100

    add tests for math square function

--- test/modules/math.test.js
+++ test/modules/math.test.js
@@ -1,6 +1,6 @@
 import test from "tape";
 
-import { cube } from "../../src/modules/math";
+import { cube, square } from "../../src/modules/math";
 
 test("math", (t) => {
   t.test("cube", (assert) => {
@@ -25,6 +25,33 @@ test("math", (t) => {
     const message = "should return single supplied argument cubed: 2";
     const expected = 8;
     const actual = cube(2);
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+
+  t.test("square", (assert) => {
+    const message = "should be a function";
+    const expected = "function";
+    const actual = typeof square;
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+
+  t.test("square", (assert) => {
+    const message = "should return single supplied argument squared: 1";
+    const expected = 1;
+    const actual = square(1);
+
+    assert.equal(actual, expected, message);
+    assert.end();
+  });
+
+  t.test("square", (assert) => {
+    const message = "should return single supplied argument squared: 2";
+    const expected = 4;
+    const actual = square(2);
 
     assert.equal(actual, expected, message);
     assert.end();
